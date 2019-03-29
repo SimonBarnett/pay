106054917aa33e8d7829d285d12c12b1a8036c33
Simon Barnett <simonbarnett@emerge-it.co.uk>
2019-03-29 12:31:35 +0000

    Add project files.

---
+++ Default.aspx
@@ -0,0 +1,122 @@
+﻿<%@ Page Language="VB" AutoEventWireup="false" CodeFile="default.aspx.vb" Inherits="_Payments" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head id="Head1" runat="server">
+    <title>Payment Portal</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <table>
+            <tr>
+                <td align="right" style="width: 213px" valign="top">
+                    Amount</td>
+                <td valign="top">
+                </td>
+                <td style="width: 224px" valign="top">
+                    <asp:TextBox ID="txtAmount" runat="server" Width="106px"></asp:TextBox>
+                    <asp:DropDownList ID="lstCurrency" runat="server">
+                        <asp:ListItem Value="GBP"></asp:ListItem>
+                        <asp:ListItem Value="EUR"></asp:ListItem>
+                        
+                    </asp:DropDownList></td>
+                <td style="width: 340px" valign="top">
+                    <asp:RequiredFieldValidator ID="RequiredFieldValidator4" runat="server" ControlToValidate="txtAmount"
+                        ErrorMessage='"Amount" missing.'></asp:RequiredFieldValidator></td>
+            </tr>
+            <tr>
+                <td align="right" style="width: 213px; height: 20px" valign="top">
+                    Card Holder Name</td>
+                <td style="width: 10px; height: 20px" valign="top">
+                </td>
+                <td style="width: 224px; height: 20px" valign="top">
+                    <asp:TextBox ID="txtCardHolder" runat="server" Width="200px"></asp:TextBox></td>
+                <td style="width: 340px; height: 20px" valign="top">
+                    <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ControlToValidate="txtAmount"
+                        ErrorMessage='"Card Holder Name" missing.'></asp:RequiredFieldValidator></td>
+            </tr>
+            <tr>
+                <td align="right" style="width: 213px; height: 24px;" valign="top">
+                    Card Type</td>
+                <td style="height: 24px" valign="top">
+                </td>
+                <td style="width: 224px; height: 24px;" valign="top">
+                    <asp:DropDownList ID="lstCardType" runat="server">
+                        <asp:ListItem Value="VISA" Text="Visa/Delta"></asp:ListItem>
+                        <asp:ListItem Value="MC" Text="Mastercard"></asp:ListItem>
+                        <asp:ListItem Value="SWITCH" Text="Switch/Solo"></asp:ListItem>
+                        <asp:ListItem Value="AMEX" Text="American Express"></asp:ListItem>
+                        <asp:ListItem Value="LASER" Text="Laser"></asp:ListItem>
+                        <asp:ListItem Value="DINERS" Text="Diners"></asp:ListItem>
+                    </asp:DropDownList></td>
+                <td style="width: 340px; height: 24px" valign="top">
+                </td>
+            </tr>
+            <tr>
+                <td align="right" style="width: 213px" valign="top">
+                    Card Number</td>
+                <td valign="top">
+                </td>
+                <td style="width: 224px" valign="top">
+                    <asp:TextBox ID="txtCardNum" runat="server" Width="200px"></asp:TextBox>
+                    <asp:Label ID="txt_CardNumHide" runat="server" Visible="False"></asp:Label></td>
+                <td style="width: 340px" valign="top">
+                    <asp:RequiredFieldValidator ID="RequiredFieldValidator3" runat="server" ControlToValidate="txtCardNum"
+                        ErrorMessage='"Card Number" missing.'></asp:RequiredFieldValidator></td>
+            </tr>
+            <tr>
+                <td align="right" style="width: 213px" valign="top">
+                    Card Expiry</td>
+                <td valign="top">
+                </td>
+                <td style="width: 224px" valign="top">
+                    <asp:DropDownList ID="lstMonth" runat="server">
+
+                        
+                    </asp:DropDownList>
+                    <asp:DropDownList ID="lstYear" runat="server">
+                    </asp:DropDownList></td>
+                <td style="width: 340px" valign="top">
+                </td>
+            </tr>
+            <tr>
+                <td align="right" style="width: 213px" valign="top">
+                    CVN</td>
+                <td valign="top">
+                </td>
+                <td style="width: 224px" valign="top">
+                    <asp:TextBox ID="txtCVN" runat="server" Width="60px"></asp:TextBox></td>
+                <td style="width: 340px" valign="top">
+                    <asp:RequiredFieldValidator ID="RequiredFieldValidator2" runat="server" ControlToValidate="txtCVN"
+                        ErrorMessage='"CVN" missing.'></asp:RequiredFieldValidator><asp:RegularExpressionValidator ID="RegularExpressionValidator1" runat="server" ControlToValidate="txtCVN"
+                        ErrorMessage='Invalid "CVN"' ValidationExpression="\d{3}"></asp:RegularExpressionValidator></td>
+            </tr>
+            <tr>
+                <td align="right" style="width: 213px; height: 20px;" valign="top">
+                </td>
+                <td style="height: 20px" valign="top">
+                </td>
+                <td style="width: 224px; height: 20px;" valign="top">
+                    <asp:Button ID="btn" runat="server" Text="Process Payment" /></td>
+                <td style="width: 340px; height: 20px" valign="top">
+                    <asp:Button ID="btnNewCard" runat="server" Text="New Card" Visible="False" /></td>
+            </tr>
+            <tr>
+                <td align="right" style="width: 213px; height: 21px" valign="top">
+                </td>
+                <td style="height: 21px" valign="top">
+                </td>
+                <td colspan="2" style="height: 21px" valign="top">
+                    <asp:Label ID="txtResult" runat="server" Width="280px"></asp:Label><br />
+                    <asp:ValidationSummary ID="ValidationSummary1" runat="server" />
+                </td>
+            </tr>
+        </table>
+    
+    </div>
+        <asp:HiddenField ID="ENV" runat="server" Visible="False" />
+    </form>
+</body>
+</html>
---
+++ default.aspx.vb
@@ -0,0 +1,241 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+Imports PriPROC6.Interface.Web
+
+Partial Class _Payments
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+        Dim ref As String = Request.Params("REF")
+        Dim iType As String = Request.Params("TYPE")
+        Dim env As String = Request.Params("ENV")
+        If IsNothing(ref) Then Throw New Exception("No Invoice reference")
+
+        If Not Page.IsPostBack Then
+            With Me
+                For m As Integer = 1 To 12
+                    .lstMonth.Items.Add(m.ToString)
+                Next
+                For y As Integer = Year(Now) To Year(Now) + 10
+                    .lstYear.Items.Add(y.ToString)
+                Next
+            End With
+        End If
+
+        Dim result As New System.Data.DataTable
+        Using cn As New SqlConnection(ConfigurationManager.ConnectionStrings("priority").ConnectionString)
+            With cn
+                .Open()
+                .ChangeDatabase(env)
+            End With
+
+            Using cmd As New SqlCommand("", cn)
+                With cmd
+                    Select Case iType
+                        Case "E"
+                            .CommandText = Replace(ConfigurationManager.AppSettings.Get("_SQLQueryE"), "%qIVNUM%", ref)
+                            With Me.btnNewCard
+                                .Visible = False
+                            End With
+
+                        Case "T"
+                            .CommandText = Replace(ConfigurationManager.AppSettings.Get("_SQLQueryT"), "%qIVNUM%", ref)
+
+                        Case "SO"
+                            .CommandText = Replace(ConfigurationManager.AppSettings.Get("_SQLQuerySO"), "%qIVNUM%", ref)
+
+                    End Select
+                    result.Load(.ExecuteReader())
+                    With result
+                        For Each row As DataRow In result.Rows
+                            For Each col As DataColumn In result.Columns
+                                Select Case col.ColumnName
+                                    Case Else
+                                        Dim o As Object = Page.FindControl(col.ColumnName)
+
+                                        Try
+                                            If Not IsNothing(o) Then
+                                                o.Text = CStr(row(col.ColumnName))
+                                            Else
+                                                Dim fld As New HiddenField()
+                                                fld.ID = col.ColumnName
+                                                fld.Value = row(col.ColumnName)
+                                                Page.Form.Controls.Add(fld)
+                                            End If
+
+                                        Catch ex As Exception
+
+                                        End Try
+
+                                End Select
+
+                            Next
+                        Next
+
+                    End With
+
+                End With
+
+            End Using
+
+        End Using
+
+    End Sub
+
+    Protected Sub Button1_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btn.Click
+
+        Me.btn.Enabled = False
+
+        Dim tReq As RealexPayments.RealAuth.TransactionRequest
+        Dim ccReq As RealexPayments.RealAuth.CreditCard
+        Dim tResp As RealexPayments.RealAuth.TransactionResponse
+
+        Dim backgroundColour As Drawing.Color
+        Dim displayMessage As String = String.Empty
+
+        Dim MerchantName As String = Nothing
+        Dim normalPassword As String = Nothing
+        Dim rebatePassword As String = Nothing
+        Dim refundPassword As String = Nothing
+        Dim transAccount As String = Nothing
+
+        With ConfigurationManager.AppSettings
+            MerchantName = .Get("MerchantName")
+            normalPassword = .Get("normalPassword")
+            rebatePassword = .Get("rebatePassword")
+            refundPassword = .Get("refundPassword")
+            transAccount = .Get("transAccount")
+        End With
+
+        Try
+
+            If IsNothing(MerchantName) Then _
+                Throw New Exception("The selected environment does not have a Realex account configured.")
+
+            Dim orderID As String
+            orderID = Now().ToString("yyyyMMddhhmmss")
+
+            With ConfigurationManager.AppSettings
+                tReq = New RealexPayments.RealAuth.TransactionRequest(
+                    MerchantName,
+                    normalPassword,
+                    rebatePassword,
+                    refundPassword
+                )
+
+                ccReq = New RealexPayments.RealAuth.CreditCard(
+                    Request("lstCardType"),
+                    txtCardNum.Text,
+                    Right("00" & Request("lstMonth"), 2) & Right("00" & Request("lstYear"), 2),
+                    txtCardHolder.Text,
+                    Request("txtCVN"),
+                    RealexPayments.RealAuth.CreditCard.CVN_PRESENT
+                )
+
+                Dim part() As String = Split(Request("txtamount"), ".")
+                If UBound(part) = 0 Then
+                    ReDim Preserve part(1)
+                    part(1) = "00"
+                Else
+                    part(1) = Left(part(1) & "00", 2)
+                End If
+
+                tResp = tReq.Authorize(transAccount,
+                    orderID,
+                    Request("lstCurrency"),
+                    CInt(part(0) & part(1)),
+                    ccReq
+                )
+            End With
+
+            If (tResp.ResultCode = 0 Or tResp.ResultCode = 200) Then
+                Try
+                    Using pay As New paymentLoading
+                        Dim p As priRow = pay.AddRow(
+                            Request.Params("REF"),
+                            Request.Params("CUSTNAME"),
+                            Request.Params("TYPE")
+                        )
+
+                        pay.TPAYMENT2.AddRow(
+                            p,
+                            "8",
+                            CDbl(txtAmount.Text),
+                            Replace(Strings.Space(Len(txtCardNum.Text) - 4) & Right(txtCardNum.Text, 4), " ", ""),
+                            "0",
+                            tResp.ResultAuthCode,
+                            tResp.ResultPASRef
+                        )
+
+                        Dim ex As Exception = Nothing
+                        pay.Post(ex)
+                        If Not TypeOf ex Is apiResponse Then Throw (ex)
+                        With TryCast(ex, apiResponse)
+                            For Each msg As apiError In .msgs
+                                If Not msg.Loaded Then
+                                    Throw New Exception(.Message)
+                                End If
+                            Next
+
+                        End With
+
+                        displayMessage = "Processed Ok."
+                        backgroundColour = Drawing.Color.Green
+
+                    End Using
+
+                Catch ex As Exception
+                    ' transaction failed
+                    displayMessage = "Transaction failed with unhandled exception " & vbCrLf & ex.Message & vbCrLf & "Auth code was: " & tResp.ResultAuthCode
+                    backgroundColour = Drawing.Color.Yellow
+                    Me.btn.Enabled = True
+
+                End Try
+
+            Else
+                Me.btn.Enabled = True
+
+                displayMessage = "Transaction failed. " & vbCrLf & "[" & tResp.ResultCode & "] " & vbCrLf & tResp.ResultMessage
+                backgroundColour = Drawing.Color.Red
+            End If
+
+        Catch ex As Exception
+            ' transaction failed
+            displayMessage = ex.Message
+            backgroundColour = Drawing.Color.Yellow
+            Me.btn.Enabled = True
+        End Try
+
+        With Me.txtResult
+            .Text = displayMessage
+            .BackColor = backgroundColour
+        End With
+
+        'Me.btn.Enabled = True
+        'Me.btn.Enabled = False
+
+    End Sub
+
+End Class
+
+Public Class paymentLoading : Inherits priForm
+
+    Private _TPAYMENT2 As priForm
+    Public Property TPAYMENT2 As priForm
+        Get
+            Return _TPAYMENT2
+        End Get
+        Set(value As priForm)
+            _TPAYMENT2 = value
+        End Set
+
+    End Property
+
+    Sub New()
+        MyBase.New("TINVOICES", "ACCNAME", "REFERENCE")
+        _TPAYMENT2 = Me.AddForm("TPAYMENT2", "PAYMENTCODE", "QPRICE")
+
+    End Sub
+
+End Class
